c539f22eb50096458e07457eafc7a686266203fe
Sydney Amaya <150975208+sydneyaamaya@users.noreply.github.com>
2025-04-09 13:20:12 -0500

    Update nova_game.py

--- COMP323/Project/nova_game.py
+++ COMP323/Project/nova_game.py
@@ -24,15 +24,134 @@ Game structure:
 -Projectile class: 
 -Level Manager:
 """
-
-
 import pygame, sys, os
 
-def gameExit():
-    pygame.quit()
-    sys.exit()
+black = (0,0,0)
+white = (255,255,255)
+light = (192,192,192)
 
-def gameLoop():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit()
+# TITLE OF CANVAS 
+pygame.display.set_caption("Nova")
+
+width = 1200 
+    
+height = 750
+
+screen = pygame.display.set_mode((width, height))
+
+TILESIZE = 32
+#15 blocks high, 20 blocks wide represented by B. P represents player
+# the dots represent empty space, the scattered B's represent obstacles
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B.................B',
+    'B...BB............B',
+    'B.................B',
+    'B.................B',
+    'B.................B',
+    'B.................B',
+    'B........P........B',
+    'B.................B',
+    'B.................B',
+    'B.................B',
+    'B.................B',
+    'B............B....B',
+    'B.................B',
+    'B.................B',
+    'BBBBBBBBBBBBBBBBBBBB'
+]
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,30))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = width / 2
+        self.rect.bottom = height - 20
+        self.speed_x = 0
+
+    def update(self):
+        self.speed_x = 0
+        self.speed_y = 0
+        key_state = pygame.key.get_pressed()
+        if key_state[pygame.K_LEFT]:
+            self.speed_x = -1
+        if key_state[pygame.K_RIGHT]:
+            self.speed_x = 1
+        if key_state[pygame.K_UP]:
+            self.speed_y = -1
+        if key_state[pygame.K_DOWN]:
+            self.speed_y = 1
+
+        self.rect.centerx += self.speed_x
+        self.rect.centery += self.speed_y
+
+        if self.rect.right > width: 
+            self.rect.right = width 
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+
+class SpriteSheet():
+    def __init__(self, image):
+        self.sheet = image
+
+    def get_sprite(self, frame, width, height, scale, color):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0,0), ((frame * width), 0, width, height))
+        image = pygame.transform.scale(image, (width * scale, height * scale))
+        image.set_colorkey(color)
+
+        return image
+
+
+class Block(pygame.sprite.Sprite): #tile map
+    def __init__(self, game, x, y):
+        self.game = game
+        self.groups = self.game.all_sprites, self.game.blocks
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x* TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.height = TILESIZE
+
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(white)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+game_sprites = pygame.sprite.Group()
+player = Player()
+game_sprites.add(player)
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((width,height))
+        self.clock = pygame.time.Clock()
+
+    def gameLoop():
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+        
+            screen.fill(white)
+            game_sprites.draw(screen)
+            pygame.display.flip()
+
+            player.update()
+
+Game.gameLoop()
+pygame.quit()
