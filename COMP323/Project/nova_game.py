c9fbea818e0d23c0e45e8190014e66c1c0851d4d
Sydney Amaya <150975208+sydneyaamaya@users.noreply.github.com>
2025-04-10 15:09:49 -0500

    Update nova_game.py

--- COMP323/Project/nova_game.py
+++ COMP323/Project/nova_game.py
@@ -153,7 +153,7 @@ class NPC(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30,30))
-        self.image.fill(255, 0, 0)
+        self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
