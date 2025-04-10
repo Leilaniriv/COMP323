1cbd64986b9b94d85ed674196f5d5ed17e58a5e0
Sydney Amaya <150975208+sydneyaamaya@users.noreply.github.com>
2025-04-10 20:02:42 +0000

    added NPC class

--- COMP323/Project/nova_game.py
+++ COMP323/Project/nova_game.py
@@ -148,9 +148,33 @@ class Projectile(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+class NPC(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,30))
+        self.image.fill(255, 0, 0)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.speed_x = 2
+        self.speed_y = 2
+    
+    def update(self):
+        self.rect.x += self.speed_x
+        if self.rect.right > width or self.rect.left < 0:
+            self.speed_x = -self.speed_x
+
+        self.rect.y += self.speed_y
+        if self.rect.bottom > height or self.rect.top < 0:
+            self.speed_y = -self.speed_y
+
+
 game_sprites = pygame.sprite.Group()
 player = Player()
 game_sprites.add(player)
+sally = NPC()
+game_sprites.add(sally)
 
 class Game:
     def __init__(self):
@@ -172,6 +196,7 @@ class Game:
             pygame.display.flip()
 
             player.update()
+            sally.update()
 
 Game.gameLoop()
 pygame.quit()
