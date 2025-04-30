1bd683062c79e78c160015eec54d3ece442b9964
Leilaniriv <leilaniriv05@gmail.com>
2025-04-29 21:03:56 -0500

    Added textboxes in carnival

--- COMP323/Project/carnival.py
+++ COMP323/Project/carnival.py
@@ -27,6 +27,7 @@ class Carnival:
 
         font = pygame.font.Font('freesansbold.ttf', 18)
         smaller_font = pygame.font.Font('freesansbold.ttf', 14)
+
         
         #carnival signs
         sign1 = pygame.Rect(width/4 - 90, height / 4 - 60, 110, 30)
@@ -62,25 +63,45 @@ class Carnival:
         #Admission / booth 1
         admission_sign_clicked = False
         admission_prompt = False
+        admission_background = pygame.Rect(width/4 - 210, height/4 - 110, 190, 15)
+                
 
         admission_booth = font.render('Get your tickets here!', True, white)
+
+        # Semi-transparent background for text
+        admission_bg = pygame.Surface((admission_background.width + 20, admission_background.height + 20), pygame.SRCALPHA)
+        admission_bg.fill((0, 0, 0, 150))
+        
 
         #Petzel Vendor / booth3
         pretzel_sign_clicked = False
         pretzel_prompt = False
         pretzel_response_yes = False
         pretzel_response_no = False
+        pretzel_background = pygame.Rect(width/4 + 90, height/4 - 110, 235, 95)
+
+        # Semi-transparent background for text
+        pretzel_bg = pygame.Surface((pretzel_background.width + 20, pretzel_background.height + 20), pygame.SRCALPHA)
+        pretzel_bg.fill((0, 0, 0, 150))
 
         pretzel_vendor = font.render('Would you like a pretzel?', True, white)
         select = font.render('Press y or n', True, white)
-        pretzel_response = font.render('Don\'t have any money!? Get out of my line!', True, white)
-        pretzel_response2 = font.render('Why are you here?! Get out of my line!', True, white)
+        pretzel_yes1 = font.render('Don\'t have any money!? ', True, white)
+        pretzel_yes2 = font.render('Get out of my line!', True, white)
+        pretzel_no1= font.render('Why are you here?!', True, white)
+        pretzel_no2= font.render('Get out of my line!', True, white)
+
 
         #ticket booth / booth4
         ticket_sign_clicked = False
         ticket_prompt = False
 
         ticket_booth = font.render('Cool Costume!', True, white)
+        ticket_background = pygame.Rect(width/4 + 90, height/4 + 290, 140, 15)
+
+        # Semi-transparent background for text
+        ticket_bg = pygame.Surface((ticket_background.width + 20, ticket_background.height + 20), pygame.SRCALPHA)
+        ticket_bg.fill((0, 0, 0, 150))
 
         slider_win = font.render('Good Job', True, white)
 
@@ -94,15 +115,17 @@ class Carnival:
 
             if booth1.collidepoint(mouse) and admission_sign_clicked:
                 if admission_prompt:
-                    self.screen.blit(admission_booth, (width/4 - 200, height/4 - 100))
+                    self.screen.blit(admission_bg, (admission_background.x - 10, admission_background.y - 10))
+                    self.screen.blit(admission_booth, admission_background)
+                    
                     
 
             #check if sign is clicked
             if booth3.collidepoint(mouse) and pretzel_sign_clicked:
-                #pretzel_prompt = True
                 if pretzel_prompt:
-                    self.screen.blit(pretzel_vendor, (width/4 + 100, height/4 - 100))
-                    self.screen.blit(select, (width/4 + 100, height/4 - 85))
+                    self.screen.blit(pretzel_bg, (pretzel_background.x - 10, pretzel_background.y - 10))
+                    self.screen.blit(pretzel_vendor, pretzel_background)
+                    self.screen.blit(select, (width/4 + 100, height/4 - 80))
                 
                     if key_pressed[pygame.K_y]:
                         pretzel_response_yes = True
@@ -111,14 +134,16 @@ class Carnival:
                         pretzel_response_no = True
 
                 if pretzel_response_yes:
-                        self.screen.blit(pretzel_response, (width/4 + 100, height/4 -70)) 
+                        self.screen.blit(pretzel_yes1, (width/4 + 100, height/4 - 60)) 
+                        self.screen.blit(pretzel_yes2, (width/4 + 100, height/4 - 40)) 
                 elif pretzel_response_no:
-                    self.screen.blit(pretzel_response2, (width/4 + 100, height/4 - 70))
+                    self.screen.blit(pretzel_no1, (width/4 + 100, height/4 - 60))
+                    self.screen.blit(pretzel_no2, (width/4 + 100, height/4 - 40))
 
             if booth4.collidepoint(mouse) and ticket_sign_clicked:
-                #ticket_sign_clicked = True
                 if ticket_prompt:
-                    self.screen.blit(ticket_booth, (width/4 + 100, height/4 + 300))
+                    self.screen.blit(ticket_bg, (ticket_background.x - 10, ticket_background.y - 10))
+                    self.screen.blit(ticket_booth, ticket_background)
                 
                 
 
